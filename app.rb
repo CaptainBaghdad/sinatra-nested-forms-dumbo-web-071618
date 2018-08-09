2535856de7a1dd669b38cb464f253f67f1d1876b
Jason Pitts <juniorjr083@gmail.com>
2018-08-09 13:09:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,32 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/'  do 
+       erb :root
+    end
+    
+    get '/new'  do 
+      
+      erb :'pirates/new'
+    end 
+    
+    
+    
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate][:name],params[:pirate][:height],params[:pirate][:weight])
+      @fisrt_ship = Ship.new(params[:pirate][:ships][:name], params[:pirate][:ships][:type],params[:pirate][:ships][:booty])
+      
+      @pirate
+      @fisrt_ship
+     # puts @pirate
+      
+      erb :'pirates/show'
+      
+      
+    end 
+    
+    
 
   end
 end
